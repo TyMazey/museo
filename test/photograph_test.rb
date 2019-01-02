aca1a4295c58dce4c134901ddab36f80a34cf66b
Ty Mazey <tymazey@gmail.com>
2019-01-02 13:11:11 -0700

    adds photograph test

--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -1,4 +1,28 @@
 require_relative 'test_helper'
 
 class PhotographTest < Minitest::Test
-end 
+
+  def setup
+    @attributes = {
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: 4,
+      year: 1954
+    }
+  end
+
+  def test_it_exsist
+    photograph = Photograph.new(@attributes)
+
+    assert_instance_of Photograph, photograph
+  end
+
+  def test_it_has_attributes
+    photograph = Photograph.new(@attributes)
+
+    assert_equal "1", photograph.id
+    assert_equal "Rue Mouffetard, Paris (Boy with Bottles)", photograph.name
+    assert_equal 4, photograph.artist_id
+    assert_equal 1954, photograph.year
+  end
+end
